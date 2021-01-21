71da6a319c5050afc06b1b1bfb0d0b4ff9b00861
gon-88 <gon.88@kakaocorp.com>
2021-01-21 09:49:14 +0900

    Add swiftable cards

--- src/App.js
+++ src/App.js
@@ -1,81 +1,99 @@
-import React from 'react';
-// import logo from './logo.svg'
-import './App.css'
-import CardList from './cards/card'
+import React, { Component } from 'react';
+import './App.css';
+import CardList from './cards/card';
 
-function App() {
+class ShiftCards extends Component {
 
-  const cardDescription = [
-    {
-      color: "red",
-      link: "red"
-    }, {
-      color: "yellow",
-      link: "yellow"
-    }, {
-      color: "green",
-      link: "green"
-    }, {
-      color: "blue",
-      link: "blue"
-    }, {
-      color: "black",
-      link: "black"
-    }, {
-      color: "red",
-      link: "red"
-    }, {
-      color: "yellow",
-      link: "yellow"
-    }, {
-      color: "green",
-      link: "green"
-    }, {
-      color: "blue",
-      link: "blue"
-    }, {
-      color: "black",
-      link: "black"
+  constructor(props) {
+    super(props);
+
+    this.cardDescription = [
+      {
+        color: "red",
+        link: "red"
+      }, {
+        color: "yellow",
+        link: "yellow"
+      }, {
+        color: "green",
+        link: "green"
+      }, {
+        color: "blue",
+        link: "blue"
+      }, {
+        color: "black",
+        link: "black"
+      }, {
+        color: "red",
+        link: "red"
+      }, {
+        color: "yellow",
+        link: "yellow"
+      }, {
+        color: "green",
+        link: "green"
+      }, {
+        color: "blue",
+        link: "blue"
+      }, {
+        color: "black",
+        link: "black"
+      }
+    ];
+
+    this.radius = 200;
+    this.tempDegree = 0;
+    this.isActive = false;
+    this.fromX = 0;
+    this.fromY = 0;
+    this.state = {
+      baseDegree: 0
     }
-  ];
+  }
 
-  const radius = 200
-  var baseDegree = 0
+  radiansToDegrees(radians) {
+    var pi = Math.PI;
+    return radians * (180 / pi);
+  }
 
-  var isMouseDown = false
-  const onMouseMove = (it) => {
-    if (isMouseDown == true) {
-      baseDegree += it.movementX / 5
-      console.log(baseDegree)
+  onmousedown = (it) => {
+    this.isActive = true;
+    this.fromX = it.screenX;
+    this.fromY = it.screenY;
+
+    console.log("UP")
+  }
+
+  onmouseup = () => {
+    this.isActive = false;
+
+    console.log("DOWN")
+  }
+
+  onmousemove = (it) => {
+    if (this.isActive) {
+      var toX = it.screenX;
+      var toY = it.screenY;
+
+      var toAngle = this.radiansToDegrees(Math.atan2(500 - toY, toX - 500)) / 10;
+      var fromAngle = this.radiansToDegrees(Math.atan2(500 - this.fromY, this.fromX - 500)) / 10;
+
+      this.tempDegree = this.tempDegree + (toAngle - fromAngle);
+      this.setState({ baseDegree: this.tempDegree });
+      // console.log("MOVE\ttempDegree: " + this.tempDegree + "\tdeltaDegree : " + deltaDegree);
     }
-    // console.log(it)
-  }
-  const onMouseUp = () => {
-    isMouseDown = false
-    console.log("Mouse up")
-  }
-  const onMouseDown = () => {
-    isMouseDown = true
-    console.log("Mouse down")
   }
 
-  return (
-    <div id="Base" className="App"
-      style={{
-        transform: "rotateX(" + baseDegree + "deg)",
-      }}
-      onMouseDown={onMouseDown}
-      onMouseUp={onMouseUp}
-      onMouseMove={onMouseMove}
-
-      onTouchStart={onMouseDown}
-      onTouchEnd={onMouseUp}
-      onTouchMove={onMouseMove}
-    >
-
-      {<CardList globalRadius={radius} cardDescription={cardDescription} baseDegree={baseDegree} />}
-    </div>
-  );
+  render() {
+    return (
+      <div id="Base" className="App"
+        onMouseUp={this.onmouseup}
+        onMouseDown={this.onmousedown}
+        onMouseMove={this.onmousemove}>
+        {<CardList globalRadius={this.radius} cardDescription={this.cardDescription} baseDegree={this.state.baseDegree} />}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default ShiftCards;
--- src/cards/card.js
+++ src/cards/card.js
@@ -15,42 +15,32 @@ function CardList({ globalRadius, cardDescription, baseDegree }) {
     var cardCount = cardDescription.length;
     var index = 0;
 
-    console.log("baseDegree : " + baseDegree)
 
     return (
-        <div
-            style={{
-                transform: "rotateZ(" + baseDegree + "deg)",
-            }}>
-            {
-                cardDescription.map((value) => {
-                    var degree = index++ * (360 / (cardCount - 1))
+        cardDescription.map((value) => {
+            var degree = baseDegree + index++ * (360 / (cardCount - 1))
 
-                    var xy = ((degree) => {
-                        return {
-                            x: midX + (globalRadius * Math.sin((-degree * (Math.PI / 180)))),
-                            y: midY + (globalRadius * Math.cos((-degree * (Math.PI / 180)))),
-                        }
-                    })(degree);
+            var xy = ((degree) => {
+                return {
+                    x: midX + (globalRadius * Math.sin((-degree * (Math.PI / 180)))),
+                    y: midY + (globalRadius * Math.cos((-degree * (Math.PI / 180)))),
+                }
+            })(degree);
 
-                    // console.log(xy);
-
-                    if (xy.y <= midY) {
-                        return (
-                            <div key={index} className="card-items"
-                                style={{
-                                    backgroundColor: value.color, transform: "translate(" + xy.x + "px, " + xy.y + "px) rotateZ(" + degree + "deg)",
-                                    zIndex: flag ? 100 : 0,
-                                    scale: flag ? 1.2 : 1
-                                }}
-                                onMouseOver={onMouseOver}
-                                onMouseOut={onMouseOut}>
-                            </div>
-                        );
-                    }
-                })
-            }
-        </div>
+            // if (xy.y <= midY) {
+            return (
+                <div key={index} className="card-items"
+                    style={{
+                        backgroundColor: value.color, transform: "translate(" + xy.x + "px, " + xy.y + "px) rotateZ(" + degree + "deg)",
+                        zIndex: flag ? 100 : 0,
+                        scale: flag ? 1.2 : 1
+                    }}
+                    onMouseOver={onMouseOver}
+                    onMouseOut={onMouseOut}>
+                </div>
+            );
+            // }
+        })
     )
 }
 
